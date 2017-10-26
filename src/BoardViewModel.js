625df46e8bb381de56acc0c81a4a226cc98b3740
joshgo <joshgo@users.noreply.github.com>
2017-10-25 22:57:02 -0400

    Change TileModel to TileViewModel

--- src/BoardViewModel.js
+++ src/BoardViewModel.js
@@ -6,7 +6,7 @@ class BoardViewModel extends Component {
 		var tiles = [];
 		for(var i = 0; i < this.props.width; i++) {
 			for(var j = 0; j < this.props.height; j++)
-					tiles.push(<TileModel row={j} col={i} size={this.props.pixels} color={'blue'}/>);
+					tiles.push(<TileViewModel row={j} col={i} size={this.props.pixels} color={'blue'}/>);
 		}
 
 		return (tiles);
